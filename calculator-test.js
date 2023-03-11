ecbee99e505202a721fc8ead1e860374b2d9888f
Jorge Barrios <lbarrios@Jorges-MacBook-Pro.local>
2023-03-11 15:44:41 -0500

    Worked out edge cases

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,28 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 300000,
+    years: 5,
+    rate: 2.4
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('5310.99')
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 1000,
+    years: 6,
+    rate: 5.4
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('16.29')
 });
 
-/// etc
+it('should calculate to NaN', () => {
+  const values = {
+    amount: 0,
+    years: 0,
+    rate: 0
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('NaN')
+})
