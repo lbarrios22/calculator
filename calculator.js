64fc3b8a64ea72cf8d3e6a19f6d45c3cd011fd90
Jorge Barrios <lbarrios@Jorges-MacBook-Pro.local>
2023-03-11 15:34:31 -0500

    Working Calculator

--- calculator.js
+++ calculator.js
@@ -17,7 +17,7 @@ function getCurrentUIValues() {
   };
 }
 
-const loanAmounts = { amount: 20000, years: 5, rate: 2.4 };
+
 
 // Get the inputs from the DOM
 // Put some default values in the inputs
@@ -26,28 +26,31 @@ function setupIntialValues() {
   const amount = document.querySelector('#loan-amount');
   const years = document.querySelector('#loan-years');
   const rate = document.querySelector('#loan-rate');
-  amount.value = loanAmounts.amount;
-  years.value = loanAmounts.years;
-  rate.value = loanAmounts.rate;
+  amount.value = 20000;
+  years.value = 5;
+  rate.value = 2.4;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
-function update() {}
+function update() {
+  return updateMonthly(calculateMonthlyPayment(getCurrentUIValues()))
+}
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  const monthlyRate = values.rate / 100 / 12;
+  const monthlyRate = (values.rate / 100) / 12;
   const totalNumPayments = Math.floor(values.years * 12);
-  let monthlyPayments = 0;
-  monthlyPayments +=
-    (values.amount * monthlyRate) /
-    (1 - Math.pow(1 + monthlyRate, -totalNumPayments)).toFixed(2);
-  return monthlyPayments;
+  return  ((monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -totalNumPayments))).toFixed(2)
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {}
+function updateMonthly(monthly) {
+  const showMonthly = document.querySelector('#monthly-payment')
+  showMonthly.innerText = `Your monthly payment is: $${monthly}`
+}
